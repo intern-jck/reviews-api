ed30e260efe1a0679575854e98d6cf47f139236b
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-07 21:32:40 -0400

    getting things setup

---
+++ MongoDB-Schema.js
@@ -0,0 +1,64 @@
+// Reviews Product Data
+{
+  "page": Number,
+  "count": Number,
+
+  "product": String,
+  "results": [
+    {
+      "review_id": Number,
+      "rating": Number,
+      "summary": String,
+      "recommend": Boolean,
+      "response": null,
+      "body": String,
+      "date": String,
+      "reviewer_name": String,
+      "helpfulness": Number,
+      "photos": [
+        {
+          "id": Number,
+          "url": String
+        },
+        // ...
+      ]
+    },
+    {},
+    // ...
+  ]
+}
+
+// Reviews Meta Data
+{
+  "product_id": String,
+  "ratings": {
+    1: Number,
+    2: Number,
+    3: Number,
+    4: Number,
+    5: Number,
+  },
+  "recommended": {
+    0: 5,
+    1: 6
+  },
+  "characteristics": {
+    "Size": {
+      "id": Number,
+      "value": String // Decimal number ex 3.5000
+    },
+    "Width": {
+      "id": Number,
+      "value": String
+    },
+    "Comfort": {
+      "id": Number,
+      "value": String
+    },
+    "Comfort": {
+      "id": Number,
+      "value": String
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,19 @@
+Start MongoDB
+sudo systemctl start mongod
+
+Verify
+sudo systemctl status mongod
+
+Starts after reboot
+sudo systemctl enable mongod
+
+Stop
+sudo systemctl stop mongod
+
+Restart
+sudo systemctl restart mongod
+
+Use Shell
+mongosh
+
+
