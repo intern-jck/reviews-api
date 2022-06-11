9dc934cdefbbf0dbd30e1b095796a59750c52cc9
Justin Kirk <justin.c.kirk@gmail.com>
2022-06-11 13:02:30 -0400

    adds new tests to solve ram issue

---
+++ src/basic/BasicModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const BasicSchema = new Schema({
+  product_id: { type: String, unique: true },
+  results: [{
+    id:  String,
+    rating:  String,
+    date: Date,
+    summary: String,
+    body: String,
+    recommend: Boolean,
+    reported: Boolean,
+    reviewer_name: String,
+    reviewer_email: String,
+    response: String,
+    helpfulness: Number,
+    photos: [{ id: String, url: String }],
+  }],
+  meta: {
+    ratings: {
+      1: Number,
+      2: Number,
+      3: Number,
+      4: Number,
+      5: Number
+    },
+    recommended: {
+      0: Number,
+      1: Number
+    },
+    characteristics: {}
+  }
+});
+
+const BasicReview = mongoose.model('Reviews', BasicSchema);
+module.exports = BasicReview;
---
+++ src/basic/scratch.js
@@ -0,0 +1,45 @@
+
+// const addReviews = (csvPath) => {
+
+  //   const t0 = performance.now();
+  //   let operations = [];
+  //   fs.createReadStream(path.resolve(__dirname, csvPath))
+  //   .pipe(csv.parse({ headers: true }))
+  //   .on('error', error => console.error(error))
+  //   .on('data', (row) => {
+
+  //     // query here
+  //     let result = {};S
+
+  //     // Build result
+  //     Object.keys(row).map((key, i) => {
+  //       if (key !== 'product_id') {
+  //         result[key] = row[key];
+  //       }
+  //     });
+
+  //     console.log(result);
+
+  //   })
+  //   .on('end', (rowCount) => {
+  //     const t1 = performance.now();
+  //     console.log(`Added ${rowCount} rows @ ${Math.round(t1 - t0)}`)
+
+  //   });
+
+  // };
+
+
+
+  // .then(() => {
+//   console.log(`Adding Photos`);
+//   addPhotos(photosCSV);
+// })
+// .then(() => {
+//   console.log(`Adding characteristics`);
+//   addCharacteristics(characteristicsCSV);
+// })
+// .then(() => {
+//   console.log(`Updating characteristics`);
+//   updateCharacteristics(reviewsCharacteristicsCSV);
+// })
