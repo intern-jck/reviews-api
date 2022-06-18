2131ffb736b1b5e1d0388c1bfbe45dc907d7ebb5
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-17 20:19:05 -0400

    uninstalled fast-csv, ignored tests

--- tests/jest/controllers.test.js
+++
@@ -1,8 +0,0 @@
-const { getReviews, addReview, getReviewsMeta, markHelpful, reportReview } = require('../../src/database/controllers.js');
-
-
-test('getReviews should return an object', () => {
-  return getReviews().then((data) => {
-    expect(typeof data).toBe('object');
-  })
-});
